92903ba20134ae532d4f2bef66b55e8ff5d3a480
Steve Smith <ssmith@atlassian.com>
2020-03-12 14:40:26 +1100

    CHET-145: Add database dump upload functionality and integration test.

--- src/main/java/com/atlassian/migration/datacenter/core/db/DatabaseExtractor.java
+++ src/main/java/com/atlassian/migration/datacenter/core/db/DatabaseExtractor.java
@@ -3,14 +3,15 @@ package com.atlassian.migration.datacenter.core.db;
 import com.atlassian.migration.datacenter.core.exceptions.DatabaseMigrationFailure;
 
 import java.io.File;
+import java.nio.file.Path;
 
 /**
  * Copyright Atlassian: 10/03/2020
  */
 public interface DatabaseExtractor
 {
-    Process startDatabaseDump(File target) throws DatabaseMigrationFailure;
-    Process startDatabaseDump(File target, Boolean parallel) throws DatabaseMigrationFailure;
+    Process startDatabaseDump(Path target) throws DatabaseMigrationFailure;
+    Process startDatabaseDump(Path target, Boolean parallel) throws DatabaseMigrationFailure;
 
-    void dumpDatabase(File to) throws DatabaseMigrationFailure;
+    void dumpDatabase(Path to) throws DatabaseMigrationFailure;
 }
