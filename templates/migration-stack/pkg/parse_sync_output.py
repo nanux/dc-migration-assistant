7ea94b7e9fdab6a269d0c0c818ca8dd3622fd8ae
Ben Partridge <bpartridge@atlassian.com>
2020-03-13 14:15:01 +1100

    CHET-158: implement parsing of error file

--- templates/migration-stack/pkg/parse_sync_output.py
+++ templates/migration-stack/pkg/parse_sync_output.py
@@ -30,7 +30,6 @@ def parseSyncOutput(output: str):
         total_bytes = total_bytes_prefix * total_bytes_multiplier
 
         files_remaining = int(big_match.group(5))
-
         calculating = big_match.group(6) is not None
 
         status = {
@@ -39,24 +38,42 @@ def parseSyncOutput(output: str):
             'total': total_bytes,
             'isCalculating': calculating
         }
-
-        return json.dumps(status)
+        return status
 
     else:
         raise ValueError('could not find sync progress in sync output {}'.format(output))
+
+def parseError(error_file_path: str) -> str:
+    with open(error_file_path) as errFile:
+        return errFile.readlines()
 
 if len(sys.argv) != 3:
     print("Usage: {} <output file> <error file>".format(sys.argv[0]))
     exit(1)
 
 output_file_path = sys.argv[1]
+error_file_path = sys.argv[2]
+
 last_line = getLastLineOfSyncOutput(output_file_path)
 
 try:
     status = parseSyncOutput(last_line)
-    print(status)
 except ValueError:
     print("could not find file progress in last line of progress file {}".format(output_file_path), file=sys.stderr)
     exit(1)
 
+try:
+    errors = parseError(error_file_path)
+    result = {
+        'progress': status,
+        'hasErrors': True,
+        'errors': errors
+    }
+except:
+    result = {
+        'progress': status,
+        'hasErrors': False
+    }
 
+print(json.dumps(result))
+exit(0)
