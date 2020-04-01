acaa8dad260766ed7a3530f016c5004104983cf0
Ben Partridge <bpartridge@atlassian.com>
2020-04-01 11:46:08 +1100

    CHET-115: create skeleton for implementation and test of AWSMigrationHelperDeploymentService

---
+++ src/main/java/com/atlassian/migration/datacenter/core/aws/infrastructure/AWSMigrationHelperDeploymentService.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2020 Atlassian
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.atlassian.migration.datacenter.core.aws.infrastructure;
+
+import com.atlassian.migration.datacenter.core.exceptions.InvalidMigrationStageError;
+import com.atlassian.migration.datacenter.spi.infrastructure.InfrastructureDeploymentStatus;
+import com.atlassian.migration.datacenter.spi.infrastructure.MigrationInfrastructureDeploymentService;
+
+import java.util.Map;
+
+/**
+ * Manages the deployment of the migration helper stack which is used to hydrate the new
+ * application deployment with data.
+ */
+public class AWSMigrationHelperDeploymentService implements MigrationInfrastructureDeploymentService {
+    @Override
+    public void deployMigrationInfrastructure(String deploymentId, Map<String, String> params) throws InvalidMigrationStageError {
+
+    }
+
+    @Override
+    public InfrastructureDeploymentStatus getDeploymentStatus() {
+        return null;
+    }
+}
