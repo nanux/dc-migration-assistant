86828ea03debe884e512feef28769b7c3803d7a7
Andy Powell <andpow@amazon.de>
2020-04-07 10:26:46 +0200

    fixing merge conflicts

--- core/src/test/java/com/atlassian/migration/datacenter/core/fs/S3UploaderIT.java
+++ core/src/test/java/com/atlassian/migration/datacenter/core/fs/S3UploaderIT.java
@@ -21,11 +21,6 @@ import cloud.localstack.docker.LocalstackDockerExtension;
 import cloud.localstack.docker.annotation.LocalstackDockerProperties;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
-<<<<<<< HEAD:src/test/java/com/atlassian/migration/datacenter/core/fs/S3UploaderIT.java
-import com.atlassian.migration.datacenter.core.aws.infrastructure.LocalStackEnvironmentVariables;
-import com.atlassian.migration.datacenter.core.fs.reporting.DefaultFileSystemMigrationErrorReport;
-=======
->>>>>>> master:core/src/test/java/com/atlassian/migration/datacenter/core/fs/S3UploaderIT.java
 import com.atlassian.migration.datacenter.core.fs.reporting.DefaultFileSystemMigrationReport;
 import com.atlassian.migration.datacenter.core.util.UploadQueue;
 import com.atlassian.migration.datacenter.spi.fs.reporting.FileSystemMigrationReport;
@@ -53,7 +48,7 @@ import static org.mockito.Mockito.when;
 
 @Tag("integration")
 @ExtendWith({LocalstackDockerExtension.class, MockitoExtension.class})
-@LocalstackDockerProperties(environmentVariableProvider = LocalStackEnvironmentVariables.class, services = {"s3"}, imageTag = "0.10.8")
+@LocalstackDockerProperties(services = {"s3"}, imageTag = "0.10.8")
 class S3UploaderIT {
     private static final String LOCALSTACK_S3_ENDPOINT = "http://localhost:4572";
     private static final String TREBUCHET_LOCALSTACK_BUCKET = "trebuchet-localstack-bucket";
