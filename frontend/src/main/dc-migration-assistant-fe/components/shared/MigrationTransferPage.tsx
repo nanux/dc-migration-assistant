99c9e9ee00f40efabbc0020c05148f3b20b2f26a
Ben Partridge <bpartridge@atlassian.com>
2020-04-08 16:40:33 +1000

    CHET-231: Implement basic structure of loading page with placeholder data. Pending buttons and divider

--- frontend/src/main/dc-migration-assistant-fe/components/shared/MigrationTransferPage.tsx
+++ frontend/src/main/dc-migration-assistant-fe/components/shared/MigrationTransferPage.tsx
@@ -1,11 +1,30 @@
 import React, { FunctionComponent } from 'react';
 import ProgressBar, { SuccessProgressBar } from '@atlaskit/progress-bar';
+import SectionMessage from '@atlaskit/section-message';
 
 export const MigrationTransferPage: FunctionComponent<{}> = ({ children }) => {
     return (
         <>
-            <h1>test</h1>
-            {children}
+            <h1>Copy Over files</h1>
+            <p>Text describing the step</p>
+            <SectionMessage
+                title="Copying over files takes ~ time depending on the size of your instance"
+                actions={[
+                    {
+                        key: 'learn',
+                        href: 'https://en.wikipedia.org/wiki/Mary_Shelley',
+                        text: 'Learn more',
+                    },
+                ]}
+            >
+                You can close this window and get back to it at any time. We recommend to start
+                planning simulatde downtime at this point of time i.e. informing your users etc.
+            </SectionMessage>
+            <h4>Phase of copying</h4>
+            <ProgressBar isIndeterminate />
+            <p>Started today</p>
+            <p>10 hours, 15 minutes elapsed</p>
+            <p>45 000 files copied</p>
         </>
     );
 };
