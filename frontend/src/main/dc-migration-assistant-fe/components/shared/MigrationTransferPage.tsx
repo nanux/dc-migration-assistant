a6bed7fb5bbe10f668a7d85c5e54233438d8d0d4
Ben Partridge <bpartridge@atlassian.com>
2020-04-08 16:28:16 +1000

    CHET-231: Create component which renders children plus extra content

---
+++ frontend/src/main/dc-migration-assistant-fe/components/shared/MigrationTransferPage.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from 'react';
+import ProgressBar, { SuccessProgressBar } from '@atlaskit/progress-bar';
+
+export const MigrationTransferPage: FunctionComponent<{}> = ({ children }) => {
+    return (
+        <>
+            <h1>test</h1>
+            {children}
+        </>
+    );
+};
