f09d6222c7ccfb000850e114a8872de309969767
Ben Partridge <bpartridge@atlassian.com>
2020-04-09 11:21:01 +1000

    implement upper container fo transfer page

--- frontend/src/main/dc-migration-assistant-fe/components/shared/MigrationTransferPage.tsx
+++ frontend/src/main/dc-migration-assistant-fe/components/shared/MigrationTransferPage.tsx
@@ -12,29 +12,39 @@ const TransferPageContainer = styled.div`
     margin-bottom: auto;
 `;
 
+const TransferContentContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    padding-bottom: 20px;
+    border-bottom: 2px solid gray;
+`;
+
 export const MigrationTransferPage: FunctionComponent<{}> = ({ children }) => {
     return (
         <TransferPageContainer>
-            <h1>Copy Over files</h1>
-            <p>Text describing the step</p>
-            <SectionMessage
-                title="Copying over files takes ~ time depending on the size of your instance"
-                actions={[
-                    {
-                        key: 'learn',
-                        href: 'https://en.wikipedia.org/wiki/Mary_Shelley',
-                        text: 'Learn more',
-                    },
-                ]}
-            >
-                You can close this window and get back to it at any time. We recommend to start
-                planning simulatde downtime at this point of time i.e. informing your users etc.
-            </SectionMessage>
-            <h4>Phase of copying</h4>
-            <ProgressBar isIndeterminate />
-            <p>Started today</p>
-            <p>10 hours, 15 minutes elapsed</p>
-            <p>45 000 files copied</p>
+            <TransferContentContainer>
+                <h1>Copy Over files</h1>
+                <p>Text describing the step</p>
+                <SectionMessage
+                    title="Copying over files takes ~ time depending on the size of your instance"
+                    actions={[
+                        {
+                            key: 'learn',
+                            href: 'https://en.wikipedia.org/wiki/Mary_Shelley',
+                            text: 'Learn more',
+                        },
+                    ]}
+                >
+                    You can close this window and get back to it at any time. We recommend to start
+                    planning simulatde downtime at this point of time i.e. informing your users etc.
+                </SectionMessage>
+                <h4>Phase of copying</h4>
+                <ProgressBar isIndeterminate />
+                <p>Started today</p>
+                <p>10 hours, 15 minutes elapsed</p>
+                <p>45 000 files copied</p>
+            </TransferContentContainer>
         </TransferPageContainer>
     );
 };
